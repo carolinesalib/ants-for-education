ce1c6fa7ea328e3f527c56c95b156bc7e88c03ab
Caroline Salib <carolinesalibc@gmail.com>
2017-08-02 20:36:22 -0300

    Ajustado chamada do jquery;

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require gentelella
+//= require gentelella-custom
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-//= require gentelella
-//= require gentelella-custom
