3669427e042ffe4b027a5f7955b585ca640828e3
Caroline Salib <carolinesalibc@gmail.com>
2017-08-02 21:47:07 -0300

    Adicionado devise para login e alterado template de login;

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+
+  layout :layout_by_resource
+
+  protected
+
+  def layout_by_resource
+    if devise_controller? && resource_name == :user
+      'devise'
+    else
+      'application'
+    end
+  end
+
 end
