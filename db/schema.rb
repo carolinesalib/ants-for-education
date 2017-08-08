aa1358fa95aa2b05c3c7095930751bf292924843
Caroline Salib <carolinesalibc@gmail.com>
2017-08-08 20:56:58 -0300

    Criado módulo para escola;

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170804222424) do
+ActiveRecord::Schema.define(version: 20170808235618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 20170804222424) do
 
   create_table "ieducar_contigurations", force: :cascade do |t|
     t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "schools", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
