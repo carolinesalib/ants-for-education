c34317686fbfb44d162eb4bb1196e167eb723d07
Caroline Salib <carolinesalibc@gmail.com>
2017-08-24 19:46:58 -0300

    Ajustado para mostrar carga horária correta em série; carolinesalib/ants-for-education#1

--- app/models/serie.rb
+++ app/models/serie.rb
@@ -1,2 +1,5 @@
 class Serie < ApplicationRecord
+  def course_load_humanize
+    TimeConverter.min2hour(course_load)
+  end
 end
---
+++ app/services/time_converter.rb
@@ -0,0 +1,5 @@
+class TimeConverter
+  def self.min2hour(min)
+    min/60
+  end
+end
