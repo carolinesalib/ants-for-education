454fd2897e97bc0ce2bda007094670cb1713f789
Caroline Salib <carolinesalibc@gmail.com>
2017-09-12 21:06:27 -0300

    Improved getters and setters of schedule time; carolinesalib/ants-for-education#7

--- app/models/class_schedule_step.rb
+++ app/models/class_schedule_step.rb
@@ -1,3 +1,19 @@
 class ClassScheduleStep < ApplicationRecord
   belongs_to :class_schedule
+
+  def start_at=(minutes)
+    super(TimeConverter.hour2min(minutes.to_i))
+  end
+
+  def start_at
+    TimeConverter.min2hour(super)
+  end
+
+  def end_at=(minutes)
+    super(TimeConverter.hour2min(minutes.to_i))
+  end
+
+  def end_at
+    TimeConverter.min2hour(super)
+  end
 end
