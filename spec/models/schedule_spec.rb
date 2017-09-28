bf5c25c9ff692ae6580e287b36a2feba896e0ded
Caroline Salib <carolinesalibc@gmail.com>
2017-09-28 10:13:03 -0300

    Added test for teacher restriction; * One teacher shoud not be allocated in two different time; carolinesalib/ants-for-education#22

--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Schedule, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should not creates repeated schedules for the same teacher in the same step' do
+    @classroom = Classroom.create(name: 'Class A')
+    @teacher = Teacher.create(name: 'Robson')
+    @class_schedule = ClassSchedule.create(name: 'Default')
+    @class_schedule_step = ClassScheduleStep.create(class_schedule: @class_schedule, sequence: 1)
+    @discipline = Discipline.create(name: 'Math')
+
+    @schedule_1 = Schedule.new(
+      classroom: @classroom,
+      teacher: @teacher,
+      class_schedule: @class_schedule,
+      step: @class_schedule_step.sequence,
+      discipline: @discipline)
+
+    @schedule_2 = Schedule.new(
+      classroom: @classroom,
+      teacher: @teacher,
+      class_schedule: @class_schedule,
+      step: @class_schedule_step.sequence,
+      discipline: @discipline)
+
+    expect(@schedule_1.save).to be_truthy
+    expect(@schedule_2.save).to be_falsey
+  end
 end
