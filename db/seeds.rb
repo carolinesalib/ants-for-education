4a9e54fc341af4cecb77a25220cb583d4bbaf673
Caroline Salib <carolinesalibc@gmail.com>
2018-01-28 11:37:50 -0200

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Create default i-educar configuration
+IeducarConfiguration.find_or_create_by!(url: 'http://comunidade.ieducar.com.br')
 
-IeducarConfiguration.create(url: 'http://comunidade.ieducar.com.br')
+# Create default schedule
+ClassSchedule.find_or_create_by!(name: 'Default')
