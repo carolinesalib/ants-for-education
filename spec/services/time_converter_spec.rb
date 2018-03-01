30fef0c1d01ad623262e877e18a15ff4d5b5021a
Caroline Salib <carolinesalibc@gmail.com>
2018-02-28 21:07:29 -0300

    Add spec: should return 0 when text is empty

--- spec/services/time_converter_spec.rb
+++ spec/services/time_converter_spec.rb
@@ -1,16 +1,18 @@
-require 'rails_helper'
+describe TimeConverter do
+  it 'should convert hours to minutes' do
+    time = TimeConverter.new
+    time.text = '02:30'
+    expect(time.to_min).to eq(150)
+  end
 
-RSpec.describe TimeConverter, type: :service do
-  describe "#convert" do
-    it "convert hours to minutes" do
-      time = TimeConverter.new
-      time.text = '02:30'
-      expect(time.to_min).to eq(150)
-    end
+  it 'should return 0 when text is empty' do
+    time = TimeConverter.new
+    time.text = ''
+    expect(time.to_min).to eq(0)
+  end
 
-    it "convert minutes to string hour" do
-      time = TimeConverter.new 150
-      expect(time.to_s).to eq('02:30')
-    end
+  it 'should convert minutes to string hour' do
+    time = TimeConverter.new 150
+    expect(time.to_s).to eq('02:30')
   end
 end
